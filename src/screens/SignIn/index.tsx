fcd4756e87862c1ea19044d3623abfd96e216bb0
emffor <98027423+emffor@users.noreply.github.com>
2022-03-04 11:06:41 -0300

    fix: Urls de autenticação

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';  
 import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../../components/Button';
@@ -7,29 +8,45 @@ import { SignInContent } from '../../components/SignInContent';
 
 import { styles } from './styles';
 
-type AuthResponse = {
-  type: string;
-  params: {
-    access_token: string;
-  }
-}
+// type AuthResponse = {
+//   type: string;
+//   params: {
+//     access_token: string;
+//   }
+// }
 
 export function SignIn() {
   const navigation = useNavigation();
 
   async function handleSignIn() {
+    //params
+    const CLIENT_ID = '662328926738-h03s3ieamggdr7s98qmhe8s34tfrn2pl.apps.googleusercontent.com';
+    const REDIRECT_URI = 'https://auth.expo.io/@eloanmf/rn-emfoauth2';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email'); //encodeURI - recurso envolve a string - substitui o espaço entre os dois. 
+
+    //Url de autenticação
+    //Colocado ? no final, porque precisa inserir algumas informações. params
+    //?client_id
+    
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+    const response = await AuthSession.startAsync({ authUrl });
+    console.log(response);
+
     navigation.navigate('Profile');
   }
 
   return (
     <View style={styles.container}>
       <SignInContent />
+        
+        <Button
+          title="Login with Google"
+          icon="google"
+          onPress={handleSignIn}
+        />
 
-      <Button
-        title="Entrar com Google"
-        icon="google"
-        onPress={handleSignIn}
-      />
     </View>
   );
 }
